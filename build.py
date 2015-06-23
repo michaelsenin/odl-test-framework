6fb41ec0203a150970327e1e909be9ca53225ee4
Your Name <you@example.com>
2015-06-23 13:29:14 +0000

    Add logging to script

--- build.py
+++ build.py
@@ -2,23 +2,30 @@ from config import Config
 from robot.conf import RobotSettings
 from time import sleep
 
+import logging
+import sys
 import docker
 from robot import run
 from robot.running import TestSuite
 
 cfg = Config()
 settings = RobotSettings()
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 
 class ClusteringTests(object):
 
     def __init__(self):
+        logging.info('Init docker client')
         self.docker_client = \
             docker.Client(base_url='unix://var/run/docker.sock',
                           version='1.9', timeout=10)
+        logging.info('Get list of containers')
         self.containers = self.docker_client.containers()
 
     def test_general_cluster_functionality(self):
+        logging.info('Start general test')
+
         suites = cfg.suites
         suites = map(
             lambda x: unicode('/'.join([cfg.test_repo_path, cfg.suites_path, x])), suites)
@@ -27,20 +34,25 @@ class ClusteringTests(object):
                                  ':'.join([cfg.variablefile, cfg.topology_file])])
 
         variables = cfg.variables
-
         containers = [container for container in self.containers
                       if container['Names'][0][1:] in cfg.containers]
+        if len(containers) != len(cfg.containers):
+            logging.info('Please check container names in config.cfg and check reachability of the marked containers')
+            return
 
         ips = \
             [self.docker_client.inspect_container(container)['NetworkSettings']['IPAddress']
              for container in self.containers]
 
+
+        logging.info('Iterate by containers')
+
         for container, ip in zip(containers, ips):
-            print "IP FOR CHECK: ", ip
             variables['CONTROLLER'] = ip
             name = container['Names'][0][1:]
+            logging.info('Start tests against {0} with ip {1}'.format(name, ip))
             _variables = [':'.join(param) for param in variables.iteritems()]
-            print cfg.tags
+
             run(*suites, include=cfg.tags, loglevel='DEBUG',
                 log='{0}--log.html'.format(name),
                 report='{0}--report.html'.format(name),
@@ -48,7 +60,10 @@ class ClusteringTests(object):
                 variablefile=variablefile,
                 variable=_variables)
 
+            logging.info('Stop container {0} with ip {1}'.format(name, ip))
             self.docker_client.pause(container['Id'])
+
+            logging.info('Waiting for finish of election. Sleep {0}sec'.format(cfg.upstart_timeout))
             sleep(cfg.votes_timeout)
 
             other_ips = filter(lambda x: x != ip, ips)
@@ -57,16 +72,20 @@ class ClusteringTests(object):
             for _container, _ip in zip(other_containers, other_ips):
                 variables['CONTROLLER'] = _ip
                 _name = _container['Names'][0][1:]
+                logging.info('Start tests against {0} with ip {1}'.format(_name, _ip))
                 _variables = [':'.join(param)
                               for param in variables.iteritems()]
-                run(*suites, loglevel='DEBUG', include=cfg.tags,
+                run(*suites, include=cfg.tags, loglevel='DEBUG',
                     log='{0}-{1}--log.html'.format(name, _name),
                     report='{0}-{1}--report.html'.format(name, _name),
                     output='{0}-{1}--output.html'.format(name, _name),
                     variablefile=variablefile,
                     variable=_variables)
 
+            logging.info('Unpause {0} with ip {1}'.format(name, ip))
             self.docker_client.unpause(container['Id'])
+
+            logging.info('Waiting for finish of election. Sleep {0}sec'.format(cfg.upstart_timeout))
             sleep(cfg.upstart_timeout)
 
 if __name__ == '__main__':
